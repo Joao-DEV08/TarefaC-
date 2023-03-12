27bac8c48c1470a58f2e2541d9ac392f1b053d56
João Víctor <96549237+Joao-DEV08@users.noreply.github.com>
2023-03-12 15:48:57 -0300

    Add files via upload

---
+++ Convidados/Convidados/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Convidados
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] convidados = new string[5] { "João", "Maria",
+            "Bernardo", "Alan", "Claudia" };
+
+            Console.WriteLine("Segue os conviados presentes na lista: ");
+            foreach (string pessoa in convidados)
+            {
+               Console.WriteLine(pessoa);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ MaiusculoMinusculo/MaiusculoMinusculo/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaiusculoMinusculo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List <string> frutas = new List<string> ();
+
+            frutas.Add("Maçã");
+            frutas.Add("banaNa");
+            frutas.Add("Uva");
+            frutas.Add("MelAncia");
+            frutas.Add("mangA");
+
+            frutas = frutas.ConvertAll(low => low.ToLowerInvariant());
+
+
+            foreach (string item in frutas)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
